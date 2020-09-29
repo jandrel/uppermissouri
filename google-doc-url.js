eb2cfb754de75a46212321a52d695825b5588da4
Jack Dougherty <jack.dougherty@trincoll.edu>
2020-09-29 18:03:56 -0400

    Delete google-doc-url.js

--- google-doc-url.js
+++
@@ -1,4 +0,0 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SQAXR0lWM_MbcvNsAt6hD9eE5_DYA6iV50BtWPocbZo/edit#gid=0';
-
-// insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
