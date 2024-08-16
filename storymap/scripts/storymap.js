15a449c8b92ab109efa5278f1e042c9e12905e43
Jen Andrella <31245051+jandrel@users.noreply.github.com>
2024-08-16 11:19:24 -0400

    Update storymap.js

--- storymap/scripts/storymap.js
+++ storymap/scripts/storymap.js
@@ -393,12 +393,11 @@ $(window).on('load', function() {
       }
     });
 
-
     $('#contents').append(" \
       <div id='space-at-the-bottom'> \
-        <a href='#top'>  \
-          <i class='fa fa-chevron-up'></i></br> \
-          <small>Top</small>  \
+        <a href='https://www.mappingtheuppermissouri.com/'>  \
+          <i class="fas fa-home"></i></br> \
+          <medium>Return Home</medium>  \
         </a> \
       </div> \
     ");
